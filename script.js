fcc8e560221b5e52ea174b48f4a76e31961645d7
Ramadhanrngkuti <cointreuasupli@gmail.com>
2026-04-16 19:57:38 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const message = `haii aku minta maaff ya kalo aku banyak salah sama aku ntah itu di sengaja ataupun tidak.\nmaaf juga ya kalo sama aku mungkin bikin kamu bosen atau cape atau apapun itu sayang.\n\ntapi aku banyak" terimakasih sama kamu sudah mau hadir di hidup aku yg rumit ini:).”\n\nmungkin aku belom bisa jadi yg terbaik buat kamu mungkin aku juga gagal dalam memahami kamu, jadi maaf untuk semua hal yg buat kamu kecewa ya sayang\n\n.`;
+const message = `haii aku minta maaff ya kalo aku banyak salah sama kamu ntah itu di sengaja ataupun tidak.\nmaaf juga ya kalo sama aku mungkin bikin kamu bosen atau cape atau apapun itu sayang.\n\ntapi aku banyak" terimakasih sama kamu sudah mau hadir di hidup aku yg rumit ini:).”\n\nmungkin aku belom bisa jadi yg terbaik buat kamu mungkin aku juga gagal dalam memahami kamu, jadi maaf untuk semua hal yg buat kamu kecewa ya sayang\n\n.`;
 
 function showLetter() {
   document.getElementById("introText").style.opacity = 0;
